07cd304b3cc1fa271e6377fffbb3fefe961f59ad
TheGoddessInari <thegoddessinari@gmail.com>
2019-09-03 14:46:30 -0700

    Same behavior, but remove unneeded dependencies.
    Thanks to https://www.reddit.com/user/WellMakeItSomehow for
    suggesting that regex wasn't needed, and suggesting code for
    get_drive_letter.
    
    I also stripped out the debug logging (for now), and renamed
    some variables to hopefully make it clearer what's going on.

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,10 @@
 #![deny(clippy::pedantic)]
 #![warn(rust_2018_idioms)]
 
-#[macro_use]
-extern crate lazy_static;
-
-#[macro_use]
-extern crate log;
-use env_logger;
-
 use std::env;
 use std::option::Option;
 use std::path::{Path, PathBuf};
 use std::process::{Command, ExitStatus};
-
-use regex;
-use regex::Regex;
-
-lazy_static! {
-    static ref DRIVE_LETTER: Regex = Regex::new(r#"([A-Za-z]):(.*)"#).unwrap();
-    static ref PATH_SEP: Regex = Regex::new(r#"(\\)"#).unwrap();
-    static ref USERDIR: String =
-        env::var("USERPROFILE").unwrap_or_else(|_| env::var("HOME").unwrap());
-}
 
 enum UnixPathType {
     None,
@@ -31,23 +14,21 @@ enum UnixPathType {
     Home,
 }
 
-trait CaptureExt<'t> {
-    fn map_to_str(&self, i: usize) -> &str;
-}
-impl<'t> CaptureExt<'t> for regex::Captures<'t> {
-    fn map_to_str(&self, i: usize) -> &str {
-        self.get(i).map_or("", |s| s.as_str())
-    }
-}
-
 fn remove_first(s: &str) -> Option<&str> {
     s.chars().next().map(|c| &s[c.len_utf8()..])
 }
 
+fn get_drive_letter(drive: &str) -> Option<char> {
+    let mut it = drive.chars();
+    match (it.next(), it.next()) {
+        (Some(c), Some(':')) if c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' => Some(c),
+        _ => None,
+    }
+}
+
 #[allow(clippy::needless_return)]
-fn is_unix_path(captures: &Option<regex::Captures<'_>>, string: &str) -> UnixPathType {
-    debug!("{:?}\n{}", captures, string);
-    return match captures {
+fn is_unix_path(drive_letter: Option<char>, string: &str) -> UnixPathType {
+    return match drive_letter {
         Some(_) => UnixPathType::None,
         None => {
             if string.starts_with('/') {
@@ -63,76 +44,74 @@ fn is_unix_path(captures: &Option<regex::Captures<'_>>, string: &str) -> UnixPat
     };
 }
 
-fn format_path(string: &str, drive_letter: &str, new_path: &str) -> String {
+fn backslash_to_slash(string: &str) -> String {
+    string.replace('\\', "/")
+}
+
+fn format_path(string: &str, drive_letter: char, new_path: &str) -> String {
     string
         .replace(&format!("{}:\\", drive_letter), new_path)
         .replace(&format!("{}:/", drive_letter), new_path)
 }
 
 fn convert_path(string: &str) -> String {
-    let drive_letter = DRIVE_LETTER.captures(string);
-    debug!("{:?}", drive_letter);
+    let current_drive = get_drive_letter(string);
 
     let current_dir_path = env::current_dir().unwrap_or_else(|_| -> PathBuf { PathBuf::from(".") });
     let current_dir = current_dir_path.to_str().unwrap_or("");
 
-    match is_unix_path(&drive_letter, string) {
+    match is_unix_path(current_drive, string) {
         UnixPathType::Root => {
-            if let Some(cap) = DRIVE_LETTER.captures(current_dir) {
-                let new_path = format!("/mnt/{}", cap.map_to_str(1).to_lowercase());
+            if let Some(drive_letter) = get_drive_letter(current_dir) {
+                let root_path = format!("/mnt/{}", drive_letter.to_lowercase());
                 let path = format!(
                     "{}{}",
-                    new_path,
-                    format_path(string, cap.map_to_str(1), &new_path)
+                    root_path,
+                    format_path(string, drive_letter, &root_path)
                 );
-                debug!("{:?}\n{:?}", new_path, path);
-                PATH_SEP.replace_all(&path, r#"/"#).to_string()
+                backslash_to_slash(&path)
             } else {
-                let new_path = &format!("{}{}", "/mnt", string);
-                PATH_SEP.replace_all(new_path, r#"/"#).to_string()
+                let path = &format!("{}{}", "/mnt", string);
+                backslash_to_slash(path)
             }
         }
         UnixPathType::Home => {
-            let home_path = USERDIR.to_string();
-            if let Some(cap) = DRIVE_LETTER.captures(&home_path) {
-                let new_path = format!("/mnt/{}/", cap.map_to_str(1).to_lowercase());
+            let home_path = env::var("USERPROFILE").unwrap_or_else(|_| env::var("HOME").unwrap());
+            if let Some(drive_letter) = get_drive_letter(&home_path) {
+                let root_path = format!("/mnt/{}/", drive_letter.to_lowercase());
                 let old_path = format!("{}{}", home_path, remove_first(string).unwrap_or(""));
-                let path = format_path(&old_path, cap.map_to_str(1), &new_path);
-                debug!("{:?}\n{:?}\n{:?}", new_path, old_path, path);
-                PATH_SEP.replace_all(&path, r#"/"#).to_string()
+                let path = format_path(&old_path, drive_letter, &root_path);
+                backslash_to_slash(&path)
             } else {
-                PATH_SEP.replace_all(string, r#"/"#).to_string()
+                backslash_to_slash(string)
             }
         }
-        UnixPathType::Other => PATH_SEP.replace_all(string, r#"/"#).to_string(),
+        UnixPathType::Other => backslash_to_slash(string),
         UnixPathType::None => {
-            if let Some(cap) = drive_letter {
-                let new_path = format!("/mnt/{}/", cap.map_to_str(1).to_lowercase());
-                let path = format_path(string, cap.map_to_str(1), &new_path);
-                debug!("{:?}", cap);
-                PATH_SEP.replace_all(&path, r#"/"#).to_string()
-            } else if let Some(cap) = DRIVE_LETTER.captures(current_dir) {
+            if let Some(drive_letter) = current_drive {
+                let root_path = format!("/mnt/{}/", drive_letter.to_lowercase());
+                let path = format_path(string, drive_letter, &root_path);
+                backslash_to_slash(&path)
+            } else if let Some(drive_letter) = get_drive_letter(current_dir) {
                 if string.starts_with('\\') {
-                    let new_path = format!("/mnt/{}", cap.map_to_str(1).to_lowercase());
+                    let root_path = format!("/mnt/{}", drive_letter.to_lowercase());
                     let path = format!(
                         "{}{}",
-                        new_path,
-                        format_path(string, cap.map_to_str(1), &new_path)
+                        root_path,
+                        format_path(string, drive_letter, &root_path)
                     );
-                    debug!("{:?}\n{:?}", new_path, path);
-                    PATH_SEP.replace_all(&path, r#"/"#).to_string()
+                    backslash_to_slash(&path)
                 } else {
-                    let new_path = format!("/mnt/{}/", cap.map_to_str(1).to_lowercase());
+                    let root_path = format!("/mnt/{}/", drive_letter.to_lowercase());
                     let path = format!(
                         "{}/{}",
-                        format_path(current_dir, cap.map_to_str(1), &new_path),
+                        format_path(current_dir, drive_letter, &root_path),
                         string
                     );
-                    debug!("{:?}\n{:?}", new_path, path);
-                    PATH_SEP.replace_all(&path, r#"/"#).to_string()
+                    backslash_to_slash(&path)
                 }
             } else {
-                PATH_SEP.replace_all(string, r#"/"#).to_string()
+                backslash_to_slash(string)
             }
         }
     }
@@ -155,7 +134,6 @@ fn escape(strings: &[String]) -> Vec<String> {
 }
 
 fn main() -> Result<(), ExitStatus> {
-    env_logger::init();
     let args: Vec<String> = env::args().collect();
     let status = Command::new("wsl.exe")
         .args(escape(&args))
